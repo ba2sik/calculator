2d6f5507f3eab823d78a9a470c00fc2a1b48e592
Ron <ronzano123@gmail.com>
2020-04-06 20:44:28 +0300

    division into folders, projects, and files

---
+++ Calculator/Calculator.Core/Implementation/CalculatorParser.cs
@@ -0,0 +1,12 @@
+﻿using Calculator.Core.Abstraction;
+
+namespace Calculator.Core.Implementation
+{
+    class CalculatorParser : IParser
+    {
+        public float Parse(string str)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
